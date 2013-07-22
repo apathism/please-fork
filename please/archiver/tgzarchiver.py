5adb8a85a32131bb0227e831ddf10de5c977bd84
Koryabkin Ivan <apathism@gmail.com>
2013-07-23 00:57:24 +0400

    Вроде бы, исправлен баг в tar.gz-архиваторе.

--- please/archiver/tgzarchiver.py
+++ please/archiver/tgzarchiver.py
@@ -4,15 +4,20 @@ import tarfile
 
 class TarGzipArchiver:
     def add_folder(self, directory, folder=""):
+        if not self.sbj:
+            self.sbj = tarfile.open(self.path, self.mod)
         self.sbj.add(directory, folder, recursive=True)
 
     def __init__(self, path, mod='w:gz'):
-        self.sbj = tarfile.open(path, mod)
+        self.sbj = None
         self.path = path
         self.mod = mod
 
     def add(self, src_path, dst_path='./'):
+        if not self.sbj:
+            self.sbj = tarfile.open(self.path, self.mod)
         self.sbj.add(src_path, dst_path)
 
     def close(self):
-        self.sbj.close()
+        if self.sbj:
+            self.sbj.close()
